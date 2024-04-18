beb2b29e1b16553b022b1a0e2e9bb42cd2b63dbc
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-04-17 21:01:13 -0300

    Refactor country-list component and add Country type definition

--- src/components/country-list/index.tsx
+++ src/components/country-list/index.tsx
@@ -1,36 +1,51 @@
 'use client'
 
 import { CountryCard } from '@/components/country-card'
+import { Country } from '@/types/country'
 import { SimpleGrid } from '@mantine/core'
 import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'next/navigation'
 
-export type Country = {
-  capital: string[]
-  name: {
-    common: string
-  }
-  population: number
-  region: string
-  flags: {
-    png: string
-    svg: string
-  }
+type FetchCountriesParams = {
+  name?: string
+  region?: string
 }
 
-async function fetchCountries(): Promise<Country[]> {
-  const response = await fetch(
-    'https://restcountries.com/v3.1/all?fields=name,region,population,capital,flags'
-  )
+async function fetchCountries({
+  name,
+  region,
+}: FetchCountriesParams): Promise<Country[]> {
+  const baseUrl = 'https://restcountries.com'
+  const fields = 'name,region,population,capital,flags'
 
+  let pathname: string = ''
+
+  if (name) {
+    pathname = `/v3.1/name/${name}`
+  } else if (region) {
+    pathname = `/v3.1/region/${region}`
+  } else {
+    pathname = '/v3.1/all'
+  }
+
+  const url = new URL(pathname, baseUrl)
+  url.searchParams.set('fields', fields)
+
+  const response = await fetch(url.toString())
   const data = await response.json()
 
   return data
 }
 
 export function CountryList() {
+  const searchParams = useSearchParams()
+
+  const name = searchParams.get('name') ?? ''
+  const region = searchParams.get('region') ?? ''
+
   const { data: countries } = useQuery({
-    queryKey: ['countries'],
-    queryFn: () => fetchCountries(),
+    queryKey: ['countries', name, region],
+    queryFn: () => fetchCountries({ name, region }),
   })
 
   return (
---
+++ src/types/country.ts
@@ -0,0 +1,12 @@
+export type Country = {
+  capital: string[]
+  name: {
+    common: string
+  }
+  population: number
+  region: string
+  flags: {
+    png: string
+    svg: string
+  }
+}
