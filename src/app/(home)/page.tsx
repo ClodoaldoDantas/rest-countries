011cad49c0ff75f3a7c89044ad882352cd9dd04f
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-04-18 21:29:06 -0300

    Add experimental config and remove Suspense component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react'
 import { CountryList } from './country-list'
 import { Filters } from './filters'
 import { Container } from '@mantine/core'
@@ -6,10 +5,8 @@ import { Container } from '@mantine/core'
 export default function Home() {
   return (
     <Container size="xl" component="main" py="xl">
-      <Suspense>
-        <Filters />
-        <CountryList />
-      </Suspense>
+      <Filters />
+      <CountryList />
     </Container>
   )
 }
